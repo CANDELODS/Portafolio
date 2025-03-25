543b095d8fbd5c53890bd0619f64dee9af7efcc0
Juan Sebastian Candelo Balanta <jcandelobalanta@gmail.com>
2025-03-24 20:06:03 -0500

    Se empieza a mejorar la navegación

--- build/js/app.js
+++ build/js/app.js
@@ -78,48 +78,48 @@ function mostrarImagen(ruta) {
     body.classList.add('fijar-body');
 }
 
-// MENÚ MOBILE
-const mobileMenuBtn = document.querySelector('#mobile-menu');
-const navegacionDiv = document.querySelector('.navegacion__derecha');
-// const proyectosDiv = document.querySelector('.proyectos'); // Selecciona el contenedor de proyectos
+// // MENÚ MOBILE
+// const mobileMenuBtn = document.querySelector('#mobile-menu');
+// const navegacionDiv = document.querySelector('.navegacion__derecha');
+// // const proyectosDiv = document.querySelector('.proyectos'); // Selecciona el contenedor de proyectos
 
-mobileMenuBtn.addEventListener('click', function () {
-    // Verificar si estamos en index.html
-    const isIndexPage = window.location.pathname.includes('index.html') || window.location.pathname === '/';
+// mobileMenuBtn.addEventListener('click', function () {
+//     // Verificar si estamos en index.html
+//     const isIndexPage = window.location.pathname.includes('index.html') || window.location.pathname === '/';
 
-    if (mobileMenuBtn.classList.contains('mostrar')) {
-        mobileMenuBtn.classList.remove('mostrar'); // Toggle para añadir y quitar la clase
-        navegacionDiv.classList.remove('activa');
-    } else {
-        mobileMenuBtn.classList.add('mostrar');
-        navegacionDiv.classList.add('activa');
-    }
+//     if (mobileMenuBtn.classList.contains('mostrar')) {
+//         mobileMenuBtn.classList.remove('mostrar'); // Toggle para añadir y quitar la clase
+//         navegacionDiv.classList.remove('activa');
+//     } else {
+//         mobileMenuBtn.classList.add('mostrar');
+//         navegacionDiv.classList.add('activa');
+//     }
 
-    if (navegacionDiv) {
-        navegacionDiv.addEventListener('click', function () {
-            navegacionDiv.classList.add('oculta');
+//     if (navegacionDiv) {
+//         navegacionDiv.addEventListener('click', function () {
+//             navegacionDiv.classList.add('oculta');
 
-            setTimeout(() => {
-                mobileMenuBtn.classList.remove('mostrar');
-                navegacionDiv.classList.remove('activa');
-                navegacionDiv.classList.remove('oculta');
-            }, 1000);
-        });
-    }
+//             setTimeout(() => {
+//                 mobileMenuBtn.classList.remove('mostrar');
+//                 navegacionDiv.classList.remove('activa');
+//                 navegacionDiv.classList.remove('oculta');
+//             }, 1000);
+//         });
+//     }
 
-    // Aplicar la clase .mostrar--proyectos en todas las páginas excepto index.html
-    if (!isIndexPage) {
-        mobileMenuBtn.classList.add('mostrar--proyectos');
-    } else {
-        mobileMenuBtn.classList.remove('mostrar--proyectos');
-    }
+//     // Aplicar la clase .mostrar--proyectos en todas las páginas excepto index.html
+//     if (!isIndexPage) {
+//         mobileMenuBtn.classList.add('mostrar--proyectos');
+//     } else {
+//         mobileMenuBtn.classList.remove('mostrar--proyectos');
+//     }
 
-    // Elimina la clase de mostrar en un tamaño de tablet o mayor
-    window.addEventListener('resize', function () {
-        const anchoPantalla = document.body.clientWidth;
-        if (anchoPantalla >= 768) {
-            mobileMenuBtn.classList.remove('mostrar');
-            navegacionDiv.classList.remove('activa');
-        }
-    });
-});
+//     // Elimina la clase de mostrar en un tamaño de tablet o mayor
+//     window.addEventListener('resize', function () {
+//         const anchoPantalla = document.body.clientWidth;
+//         if (anchoPantalla >= 768) {
+//             mobileMenuBtn.classList.remove('mostrar');
+//             navegacionDiv.classList.remove('activa');
+//         }
+//     });
+// });
